1a7231a4641eea111d7b7644f85129c7604878dc
moco <pps.spark.1999@gamil.com>
2021-12-22 20:40:39 +0900

    wip

--- plugins/editor.js
+++ plugins/editor.js
@@ -1,6 +1,10 @@
+import ImageTool from '@editorjs/image'
 import EditorJS from '@editorjs/editorjs'
 import Header from '@editorjs/header'
 import List from '@editorjs/list'
+import Underline from '@editorjs/underline'
+const LinkTool = require('@editorjs/link')
+const Marker = require('@editorjs/marker')
 
 export default ({ $axios }, inject) => {
   // 非同期通信が必要なeditorjsのpluginは $axios を使う必要がありますが、header/listプラグインは不要です。
@@ -14,15 +18,37 @@ export default ({ $axios }, inject) => {
           header: {
             class: Header,
             inlineToolbar: true,
-            shortcut: 'CMD+SHIFT+H',
+            levels: [1, 2, 3, 4, 5, 6],
+            defaultLevel: 1,
+            shortcut: 'CMD+SHIFT+H'
           },
           list: {
             class: List,
             inlineToolbar: true,
-            shortcut: 'CMD+SHIFT+L',
+            shortcut: 'CMD+SHIFT+L'
           },
-        },
+          underline: Underline,
+          inkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: 'http://localhost:4000/posts' // Your backend endpoint for url data fetching
+            }
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              endpoints: {
+                byFile: 'http://localhost:4000/uploadFile', // Your backend file uploader endpoint
+                byUrl: 'http://localhost:4000/posts' // Your endpoint that provides uploading by Url
+              }
+            }
+          },
+          Marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M'
+          }
+        }
       })
-    },
+    }
   })
 }
