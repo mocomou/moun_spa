8c1cf52e4d13e01b1e298342885264c952134278
moco <pps.spark.1999@gamil.com>
2021-12-25 08:35:08 +0900

    switch btn css

--- components/atoms/button.vue
+++ components/atoms/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="btn">
+  <button>
     {{ character }}
   </button>
 </template>
@@ -12,22 +12,11 @@ export default {
       require: true
     }
   }
-  // data () {
-  //   return {
-  //     character: ''
-  //   }
-  // },
-  // methods: {
-  //   btnCharacter: (text) => {
-  //     const character = text
-  //     return character
-  //   }
-  // }
 }
 </script>
 
-<style scoped lang="scss">
-.btn {
+<style lang="scss">
+.primary {
   color: $base-font-color;
   border: 2px solid $base-font-color;
   background-color: white;
@@ -42,8 +31,30 @@ export default {
   transition: all 0.3s;
 }
 
-.btn:hover {
+.primary:hover {
   background-color: $base-font-color;
+  color: white;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+.secondary {
+  color: $secondary-font-color;
+  border: 2px solid $secondary-font-color;
+  background-color: white;
+  font-size: 0.8rem;
+  font-weight: bold;
+  padding: 0.7rem 0.9rem;
+  margin: 10px 0;
+  width: $btn-width;
+  display: flex;
+  justify-content: center;
+  border-radius: 4px;
+  transition: all 0.3s;
+}
+
+.secondary:hover {
+  background-color: $secondary-font-color;
   color: white;
   cursor: pointer;
   transition: all 0.3s;
