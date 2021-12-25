e312fa4457af19ccf0b04604979b11114d117b4b
moco <pps.spark.1999@gamil.com>
2021-12-25 22:21:20 +0900

    modify btn css

--- components/atoms/button.vue
+++ components/atoms/button.vue
@@ -32,18 +32,19 @@ export default {
 }
 
 .primary:hover {
-  background-color: black;
+  // background-color: black;
   // color: $base-font-color;
   // border: 2px solid $base-font-color;
-  // opacity: 0.9;
+  opacity: 0.8;
   cursor: pointer;
   transition: all 0.4s;
 }
 
 .secondary {
-  color: #7d7d7d;
-  border: 2px solid $secondary-font-color;
-  background-color: white;
+  color: white;
+  border: none;
+  // border: 2px solid $secondary-font-color;
+  background-color: $secondary-font-color;
   font-size: 0.8rem;
   font-weight: bold;
   padding: 0.7rem 0.9rem;
@@ -56,8 +57,9 @@ export default {
 }
 
 .secondary:hover {
-  background-color: $secondary-font-color;
-  color: white;
+  // background-color: $secondary-font-color;
+  // color: white;
+  opacity: 0.8;
   cursor: pointer;
   transition: all 0.3s;
 }
