38522b8747c812df63ccab35636ee81bb32af31a
moco <pps.spark.1999@gamil.com>
2022-01-02 16:48:39 +0900

    add callback

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    foobar
-  </div>
+  <p class="login">
+    Logging in...
+  </p>
 </template>
 
 <script>
 export default {
 }
 </script>
+
+<style scoped lass="scss">
+  .login {
+    font-size: 1.5rem;
+    text-align: center;
+    color: gray;
+    padding-top: 100px;
+  }
+</style>
