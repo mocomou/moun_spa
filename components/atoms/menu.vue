f2a3e9da1b3b8872b9f849f56a29b91a8c43b69c
moco <pps.spark.1999@gamil.com>
2022-01-03 21:16:39 +0900

    wip

--- components/atoms/menu.vue
+++ components/atoms/menu.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="menu">
+    <!--
     <button
       class="header__menu"
       @click="active=!active"
     >
       <font-awesome-icon :icon="['fas', 'bars']" class="header__menu-hamburger"/>
     </button>
+    -->
+    <button
+      class="hamburger"
+      @click="active=!active"
+    >
+      <span
+        class="hamburger__line"
+        :class="{ open: active }"
+      >
+        <span class="hamburger__name">
+          メニューを開閉する
+        </span>
+      </span>
+    </button>
     <!-- <button
       class="header__menu"
     >
       <font-awesome-icon :icon="['fas', 'times']" class="header__menu-cross" />
     </button> -->
+    <!--
     <nav v-show="active" class="menu__nav">
+    -->
+    <nav
+      class="menu__nav"
+      :class="{ open: active }"
+     >
       <ul class="menu__list">
         <!-- logout -->
         <template v-if="!loggedIn">
@@ -70,12 +91,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.menu {
+  position: relative;
+}
+
 .header__menu {
   color: white;
 }
 
 .menu__nav {
   background-color: #404040;
+  position: fixed;
+  top: $header-height;
+  // top: -100px;
+  left: 0;
+  width: 100%;
+  transition: all 1s;
+  opacity: 0;
+}
+
+.menu__nav.open {
+  top: $header-height;
+  opacity: 1;
 }
 
 .menu__item {
@@ -83,7 +120,63 @@ export default {
 }
 
 .menu__link {
+  display: block;
   color: white;
   text-decoration: none;
+  padding: 8px 8px 8px 40px;
+}
+
+$menuSize: 16px;
+$menuLineHeight: 2px;
+
+.hamburger {
+  position: relative;
+  width: $menuSize;
+  height: $menuSize;
+}
+
+.hamburger__line,
+.hamburger__line::after,
+.hamburger__line::before {
+  display: block;
+  background-color: white;
+  width: $menuSize;
+  height: $menuLineHeight;
+  transition: all .5s;
+}
+
+.hamburger__line::after,
+.hamburger__line::before {
+  position: absolute;
+  content: '';
+}
+
+.hamburger__line::before {
+  top: 0;
+}
+
+.hamburger__line::after {
+  bottom: 0;
+}
+
+.hamburger__line.open {
+  background-color: transparent;
+}
+
+.hamburger__line.open::before {
+  top: 50%;
+  transform: translateY(-50%) rotate(-45deg);
+}
+
+.hamburger__line.open::after {
+  top: 50%;
+  transform: translateY(-50%) rotate(45deg);
+}
+
+.hamburger__name {
+  display: block;
+  width: 0px;
+  height: 0px;
+  visibility: hidden;
 }
 </style>
